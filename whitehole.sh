3eab9405490312ebe80d5296e99c5c52a086cd17
jplusc <jam.git@kastlerock.com>
2019-10-23 09:36:22 -0400

    Update whitehole.sh

--- whitehole.sh
+++ whitehole.sh
@@ -26,16 +26,18 @@ curl -sS https://raw.githubusercontent.com/jplusc/whitehole/master/whitelist.txt
 curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt | grep -o '^[^#]*' >> "${PIHOLE_LOCATION}"/whitelist.new
 
 echo -e "  [ ] Adding domains to whitelist... "
-cat "${PIHOLE_LOCATION}"/whitelist.new >> "${PIHOLE_LOCATION}"/whitelist.txt
+# I don't fully understand why the tee is needed, but I can't get permission to write to whitelist.txt on output of
+# cat/sort even if I toss a sudo before it (which I don't want to do anyway)
+# ok, i would need to sudo the file operator ">" or ">>" but you really can't do that, so you need to use tee which you can sudo
+#cat "${PIHOLE_LOCATION}"/whitelist.new > "${PIHOLE_LOCATION}"/whitelist.txt
+cat "${PIHOLE_LOCATION}"/whitelist.new | tee "${PIHOLE_LOCATION}"/whitelist.txt > /dev/null
 rm "${PIHOLE_LOCATION}"/whitelist.new 2> /dev/null
 
 echo -e "  [ ] Removing duplicates... "
 mv "${PIHOLE_LOCATION}"/whitelist.txt "${PIHOLE_LOCATION}"/whitelist.tmp 
 # the "awk '{$1=$1;print}'" is to remove any leading or trailing whitespace so the will be properly de-duped.
-# I don't fully understand why the tee is needed, but I can't get permission to write to whitelist.txt on output of
-# sort even if I toss a sudo before it (which I don't want to do anyway)
 #cat "${PIHOLE_LOCATION}"/whitelist.tmp | awk '{$1=$1;print}' | sort -f -u > "${PIHOLE_LOCATION}"/whitelist.txt
-cat "${PIHOLE_LOCATION}"/whitelist.tmp | awk '{$1=$1;print}' | sort -f -u | tee -a "${PIHOLE_LOCATION}"/whitelist.txt > /dev/null
+cat "${PIHOLE_LOCATION}"/whitelist.tmp | awk '{$1=$1;print}' | sort -f -u | tee "${PIHOLE_LOCATION}"/whitelist.txt > /dev/null
 rm "${PIHOLE_LOCATION}"/whitelist.tmp 2> /dev/null
 
 echo -e "  [ ] Pi-hole gravity rebuilding lists. This may take a while... "
